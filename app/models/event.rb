43e85304e025ca9bb1673e14a24e0e55c3b30869
Ruthan <rcfreese@gmail.com>
2014-06-17 17:36:37 -0400

    Add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+  validates :title, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
+  validates :description, length: { maximum: 140 }
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,7 @@
 class School < ActiveRecord::Base
+  validates :name, presence: true
+  validates :year_start, presence: true, numericality: { only_integer: true }
+  validates :year_end, presence: true,
+                       numericality: { only_integer: true,
+                                       greater_than_or_equal_to: :year_start }
 end
